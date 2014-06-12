67f0c99c8585f9f613734d65f01debcb23267eda
ebrandner <brandner.erik@gmail.com>
2014-06-12 15:06:19 +0200

    Abstimm-Screen angepasst

--- TeamPlanBuch/src/de/iconiaone/teamplanbuch/SingleItem.java
+++ TeamPlanBuch/src/de/iconiaone/teamplanbuch/SingleItem.java
@@ -5,13 +5,26 @@ package de.iconiaone.teamplanbuch;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
  
-public class SingleItem extends Activity{
+public class SingleItem extends Activity implements OnClickListener{
+	
+	Button zusageButton;
+	Button absageButton;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.single_list_item_view);
+        
+        zusageButton = (Button) findViewById(R.id.zusageButton);
+		zusageButton.setOnClickListener(this);   
+		absageButton = (Button) findViewById(R.id.absageButton);
+		absageButton.setOnClickListener(this);   
          
         TextView txtProduct = (TextView) findViewById(R.id.product_label);
          
@@ -22,4 +35,21 @@ public class SingleItem extends Activity{
         txtProduct.setText(product);
          
     }
+
+	@Override
+	public void onClick(View v) {
+		if (v == zusageButton)
+		{
+			Intent intent = new Intent(this, HomeScreen.class);
+		    startActivity(intent);	
+			Toast.makeText(getApplicationContext(), "Termin bestätigt", Toast.LENGTH_LONG).show();
+		}
+		
+		if (v == absageButton)
+		{
+			Intent intent = new Intent(this, HomeScreen.class);
+		    startActivity(intent);	
+			Toast.makeText(getApplicationContext(), "Termin abgelehnt", Toast.LENGTH_LONG).show();
+		}
+	}
 }
